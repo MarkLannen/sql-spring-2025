644cf7ffb5e12f59bd7854065fa5bb0eae709d7d
Mark Lannen <marklannendev@gmail.com>
2025-02-03 21:23:38 -0700

    add first draft of 3.5

--- Module-3/module-3-assignment.sql
+++ Module-3/module-3-assignment.sql
@@ -40,3 +40,30 @@ ORDER BY total_spend DESC
 LIMIT 3
 
 -- 3.5
+WITH avg_spend_55406 AS(
+SELECT t.month AS month, 
+	ROUND (AVG(t.spend)) AS "Avg Spend 05"
+FROM owner_year_month AS t
+INNER JOIN owners as o
+	ON o.card_no = t.card_no
+WHERE o.zip = "55405"
+GROUP BY t.month), 
+avg_spend_55408 AS (
+SELECT t.month, 
+	ROUND (AVG(t.spend)) AS "Avg Spend 08"
+FROM owner_year_month AS t
+INNER JOIN owners AS o
+	ON o.card_no = t.card_no
+WHERE o.zip = "55408"
+GROUP BY t.month),
+avg_spend_55403 AS (
+SELECT t.month ,
+	ROUND(AVG(t.spend)) AS "Avg Spend 03"
+FROM owner_year_month AS t
+INNER JOIN owners AS o
+	ON o.card_no = t.card_no
+WHERE o.zip = "55403"
+GROUP BY t.month)
+SELECT t03.month, "Avg Spend 03", "Avg Spend 05", "Avg Spend 08"
+FROM avg_spend_55403 AS t03
+INNER JOIN avg_spend_55405 AS t05 ON t03.month = t05.month
