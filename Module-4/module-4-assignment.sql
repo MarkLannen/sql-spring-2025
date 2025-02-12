ee0b76458934571806bb4cc826252deae63676b1
Mark.Lannen <mark.lannen@mso.umt.edu>
2025-02-12 15:56:27 -0700

    add 4.1 and 4.2

---
+++ Module-4/module-4-assignment.sql
@@ -0,0 +1,19 @@
+
+-- 4.1
+DROP TABLE IF EXISTS product_summary;
+
+CREATE TABLE product_summary (
+    year INTEGER,
+    description TEXT,
+    sales NUMERIC
+);
+
+-- 4.2
+INSERT INTO product_summary (year, description, sales)
+VALUES
+    (2014, 'BANANA Organic', 176818.73),
+    (2015, 'BANANA Organic', 258541.96),
+    (2014, 'AVOCADO Hass Organic', 146480.34),
+    (2014, 'ChickenBreastBoneless/Skinless', 204630.90);
+
+SELECT * FROM product_summary;
