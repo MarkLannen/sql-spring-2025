78aeed970d8c7643734598293fc33d23a62d0f37
Mark Lannen <marklannendev@gmail.com>
2025-02-16 13:49:20 -0700

    add 5.2

--- Module-5/module-5-assignment.sql
+++ Module-5/module-5-assignment.sql
@@ -3,3 +3,18 @@ SELECT SUM(spend) AS total_spend
 FROM owner_spend_date;
 
 -- 5.2
+WITH high_spenders AS (
+    SELECT owner_spend_date.card_no
+    FROM owner_spend_date
+    WHERE SUBSTR(owner_spend_date.spend, 1, 4) = '2017'
+    GROUP BY owner_spend_date.card_no
+    HAVING SUM(owner_spend_date.
+	spend) > 10000
+)
+
+SELECT owner_spend_date.card_no, MAX(owner_spend_date.spend) AS spend
+FROM owner_spend_date
+JOIN high_spenders ON owner_spend_date.card_no = high_spenders.card_no
+WHERE owner_spend_date.card_no != 3
+GROUP BY owner_spend_date.card_no
+ORDER BY spend DESC;
