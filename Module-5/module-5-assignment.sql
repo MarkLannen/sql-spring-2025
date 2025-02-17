90d30721f4436824efa5b3a21c47f27acab8cae9
Mark Lannen <marklannendev@gmail.com>
2025-02-17 13:24:53 -0700

    add 5.6

--- Module-5/module-5-assignment.sql
+++ Module-5/module-5-assignment.sql
@@ -74,3 +74,33 @@ GROUP BY
 ORDER BY
     avg_spent_per_transaction DESC
 LIMIT 5;
+
+-- 5.6
+SELECT
+    owners.zip AS "Zip",
+    COUNT(DISTINCT owners.card_no) AS "Number of Owners",
+    AVG(owner_spend.total_spend) AS "Avg Spent Per Owner",
+    SUM(owner_spend_date.spend) / COUNT(owner_spend_date.card_no) AS "Avg Spend Per Transaction"
+FROM
+    owners
+JOIN
+    owner_spend_date ON owners.card_no = owner_spend_date.card_no
+JOIN (
+    SELECT card_no, SUM(spend) AS total_spend
+    FROM owner_spend_date
+    GROUP BY card_no
+) AS owner_spend ON owners.card_no = owner_spend.card_no
+WHERE
+    owners.zip IN (
+        SELECT zip
+        FROM owners
+        GROUP BY zip
+        HAVING COUNT(DISTINCT card_no) >= 100
+    )
+GROUP BY
+    owners.zip
+ORDER BY
+    "Avg Spend Per Transaction" ASC
+LIMIT 5;
+
+
