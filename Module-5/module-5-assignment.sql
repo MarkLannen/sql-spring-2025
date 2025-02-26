f073f6c3391e21af6782ad2eadf2382959a47545
Mark Lannen <marklannendev@gmail.com>
2025-02-25 20:45:41 -0700

    add 5.9 and 5.10

--- Module-5/module-5-assignment.sql
+++ Module-5/module-5-assignment.sql
@@ -73,4 +73,40 @@ CREATE TABLE IF NOT EXISTS owner_products (
 conn.commit()
 conn.close()
 
+-- 5.9
+
+owner_prod = []
+with open("owner_products.txt", 'r') as infile:
+    next(infile)
+    for line in infile:
+        line = line.strip().split("\t")
+        owner_prod.append(line)
+
+c.executemany('''
+INSERT INTO owner_products (owner, upc, description, dept_name, spend, items, trans)
+VALUES (?, ?, ?, ?, ?, ?, ?)
+''', owner_prod)
+
+conn.commit()
+conn.close()
+
+-- 5.10
+
+conn = sqlite3.connect('owner_prod.db')
+c = conn.cursor()
+
+query = '''
+SELECT description, dept_name, SUM(spend) AS total_spend
+FROM owner_products
+WHERE dept_name LIKE '%groc%'
+GROUP BY description, dept_name
+ORDER BY total_spend DESC
+'''
+
+c.execute(query)
+
+for i in range(10):
+    print(rows[i])
+
+conn.close()
 
